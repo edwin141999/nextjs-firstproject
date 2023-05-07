d23eb4ad95a64aa4a91d1e7452239297dfc62e5d
edwin141999 <gendevilman@gmail.com>
2023-05-07 15:15:09 -0600

    Navegacion

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas tempore necessitatibus blanditiis dolorum id voluptatibus quod nesciunt quaerat consequatur aspernatur perspiciatis et saepe nihil magnam deleniti eaque veritatis eius officiis dolores, similique nulla maiores eveniet tenetur? Ratione omnis fugiat nesciunt consectetur rem dolores sed error quasi ut aliquid sit asperiores fugit corrupti maxime eos provident modi earum, iusto sint a architecto dignissimos in! In, dolore officiis tempore similique, iste, cumque dolor cupiditate et quam quia aut obcaecati. Molestias dolor aspernatur sit itaque distinctio nihil nisi maiores odio minima nostrum commodi placeat, iste quod a ducimus facilis consequatur? Nemo, facilis perspiciatis?</p>
+        </div>
+    );
+}
