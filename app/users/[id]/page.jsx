592e8c98c0b14fe978817dc165dfe9bc46d006f5
edwin141999 <gendevilman@gmail.com>
2023-05-08 13:57:03 -0600

    user details

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,3 +1,23 @@
-export default function UsersPage() {
-    return <div>User details</div>;
+async function getUser(id) {
+    const res = await fetch(`https://reqres.in/api/users/${id}`);
+    const data = await res.json();
+    return data.data;
+}
+
+export default async function UsersPage({ params }) {
+    const user = await getUser(params.id);
+    return (
+        <div>
+            <h1>User details</h1>
+            <div>
+                <img src={user.avatar} alt={user.email} />
+                <div>
+                    <h3>
+                        {user.id} {user.first_name} {user.last_name}
+                    </h3>
+                    <p>{user.email}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
