13739cbe8582d2f9a6c2da792ecf6df11afe811e
edwin141999 <gendevilman@gmail.com>
2023-05-08 13:48:01 -0600

    client component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
-export default function IndexPage() {
-  return (
-    <div>
-      <h1>Index Page</h1>
-    </div>
-  )
+import Users from "@/components/Users";
+
+async function fetchUsers() {
+    const res = await fetch("https://reqres.in/api/users");
+    const data = await res.json();
+    return data.data;
+}
+
+export default async function IndexPage() {
+    const users = await fetchUsers();
+    console.log(users);
+    return (
+        <div>
+            <h1>Index Page</h1>
+            <Users users={users} />
+        </div>
+    );
 }
